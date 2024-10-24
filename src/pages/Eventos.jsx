deaa356bcd7f427acd1e8663279677fd7cce17c3
HulkyZeus <u20212200102@usco.edu.co>
2024-10-24 10:14:02 -0500

    Actualizacion prueba Maria Jose

---
+++ src/pages/Eventos.jsx
@@ -0,0 +1,20 @@
+import '../css/main.css';
+import styled from '@emotion/styled';
+import { Layout, Flex, Row, Col } from 'antd';
+import { Content } from 'antd/es/layout/layout';
+
+const Eventos = () => {
+  return (
+    <>
+    <Layout>
+      <Content>
+      <Row style={{ margin: '5px 0px' }}>
+
+      </Row>
+      </Content> 
+    </Layout>
+    </>
+  );
+};
+
+export default Eventos

a5ffb7c0918f7618699c581d2c0f4ed59fff1af5
Ananagl <naglessofia8@gmail.com>
2024-11-15 00:25:01 -0500

    Avances pasteleria

--- src/pages/Pasteleria.jsx
+++ src/pages/Pasteleria.jsx
@@ -1,42 +1,42 @@
 import "../css/main.css";
 import { Card, Col, Layout, Row } from 'antd';
-import Croissant from '../img/croissant.png'
-import PanQue from '../img/pandequeso.png'
-import Brownie from '../img/brownie.png'
-import PanBrioche from '../img/panbrioche.png'
-import PanGalleta from '../img/pangalleta.png'
-import Cucas from '../img/cucas.png'
-import Cañas from '../img/cañas.png'
-import Panzerotti from '../img/panzerotti.png'
-import PanMasaMadre from '../img/panmasamadre.png'
-import PanHawaiano from '../img/panhawaiano.png'
-import Pandebono from '../img/pandebono.png'
-import Almojabanas from '../img/almojabanas.png'
-import Mogollas from '../img/mogolla.png'
-import PanAliñado from '../img/panaliñado.png'
-import MojiconQueso from '../img/mojiconconqueso.png'
-import PanBatido from '../img/panbatido.png'
+import BrazoReina from '../img/brazodereina.png'
+import Tresleches from '../img/tresleches.png'
+import Ponque from '../img/ponque.png'
+import TortaZana from '../img/tortazanahoria.png'
+import TortaChoco from '../img/tortachoco.png'
+import TortaAmapola from '../img/tortaamapola.png'
+import TortaRed from '../img/tortared.png'
+import Cupcakes from '../img/cupcakes.png'
+import TortaFrutRojos from '../img/tortafrutrojos.png'
+import HojaldrePollo from '../img/hojaldrepollo.png'
+import Milhojas from '../img/milhojas.png'
+import CheesecakeFrutos from '../img/cheesecakefrutos.png'
+import TortaAlmojabana from '../img/tortaalmojabana.png'
+import EmpanadaPollo from '../img/empanadapollo.png'
+import EmpanadaCarne from '../img/empanadacarne.png'
+import TortaMilky from '../img/tortamilky.png'
 
 
 const { Content } = Layout;
 
 const products = [
-  { title: "Brazo de Reina", img: Croissant, description:"Delicioso y recién horneado"},
-  { title: "Torta de Tres Leches", img: PanBrioche, description:"Delicioso y recién horneado" },
-  { title: "Ponqué Trandicional", img: PanQue, description:"Delicioso y recién horneado" },
-  { title: "Torta de Red Velvet", img: PanGalleta, description:"Delicioso y recién horneado" },
-  { title: "Torta de Zanahoria", img: Cucas, description:"Delicioso y recién horneado" },
-  { title: "Torta de Chocolate", img: Cañas, description:"Brownies melcochudos" },
-  { title: "Torta de Naranja con Amapola", img: Panzerotti, description:"Delicioso y recién horneado" },
-  { title: "Milhoja de Arequipe", img: PanMasaMadre, description:"Delicioso y recién horneado" },
-  { title: "Cheesecake de Frutos Rojos", img: PanHawaiano, description:"Delicioso y recién horneado" },
-  { title: "Caja de Cupcakes x4", img: Pandebono, description:"Delicioso y recién horneado" },
-  { title: "Torta de Almojabana", img: Almojabanas, description:"Delicioso y recién horneado" },
-  { title: "Torta de Frutos Rojos", img: Mogollas, description:"Delicioso y recién horneado" },
-  { title: "Torta de Milky Way", img: PanAliñado, description:"Delicioso y recién horneado" },
-  { title: "Hojaldre de Pollo", img: MojiconQueso, description:"Delicioso y recién horneado" },
-  { title: "Empanada de Pollo", img: Brownie, description:"Delicioso y recién horneado" },
-  { title: "Empanada de Carne", img: PanBatido, description:"Delicioso y recién horneado" },
+  { title: "Brazo de Reina", img: BrazoReina, description:"Delicioso y recién horneado"},
+  { title: "Torta de Tres Leches", img: Tresleches, description:"Delicioso y recién horneado" },
+  { title: "Ponqué Trandicional", img: Ponque, description:"Delicioso y recién horneado" },
+  { title: "Torta de Red Velvet", img: TortaRed, description:"Delicioso y recién horneado" },
+  { title: "Torta de Zanahoria", img: TortaZana, description:"Delicioso y recién horneado" },
+  { title: "Torta de Chocolate", img: TortaChoco, description:"Brownies melcochudos" },
+  { title: "Torta de Naranja con Amapola", img: TortaAmapola, description:"Delicioso y recién horneado" },
+  { title: "Milhoja de Arequipe", img: Milhojas, description:"Delicioso y recién horneado" },
+  { title: "Cheesecake de Frutos Rojos", img: CheesecakeFrutos, description:"Delicioso y recién horneado" },
+  { title: "Caja de Cupcakes x4", img: Cupcakes, description:"Delicioso y recién horneado" },
+  { title: "Torta de Almojabana", img: TortaAlmojabana, description:"Delicioso y recién horneado" },
+  { title: "Torta de Frutos Rojos", img: TortaFrutRojos, description:"Delicioso y recién horneado" },
+  { title: "Torta de Milky Way", img: TortaMilky, description:"Delicioso y recién horneado" },
+  { title: "Hojaldre de Pollo", img: HojaldrePollo, description:"Delicioso y recién horneado" },
+  { title: "Empanada de Pollo", img: EmpanadaPollo, description:"Delicioso y recién horneado" },
+  { title: "Empanada de Carne", img: EmpanadaCarne, description:"Delicioso y recién horneado" },
 ];
 
 const Pasteleria = () => {
@@ -44,8 +44,11 @@ const Pasteleria = () => {
     <>
     <Layout>
       <Content>
-      <h1>Pastelería</h1>
-      <p>Bienvenido a la sección de pastelería.</p>
+
+      <div gutter={[16, 16]} justify="center" style={{ marginTop: "30px", textAlign: "center" }}>
+        <h1>Pastelería</h1>
+        <p>Bienvenido a la sección de pastelería.</p>
+      </div>
       
       <Row gutter={[16, 16]} justify="center" style={{ marginTop: "30px" }}>
         {products.slice(0, 4).map((product, index) => (

f3041c4d37cab35348a50b59298c455cde204758
HulkyZeus <u20212200102@usco.edu.co>
2024-11-20 17:38:28 -0500

    Arreglos minimos

--- src/pages/Panaderia.jsx
+++ src/pages/Panaderia.jsx
@@ -17,6 +17,7 @@ import Mogollas from '../img/mogolla.png';
 import PanAliñado from '../img/panaliñado.png';
 import MojiconQueso from '../img/mojiconconqueso.png';
 import PanBatido from '../img/panbatido.png';
+import { useTranslation } from "react-i18next";
 import FondoPan from '../img/FondoPan.webp'
 
 

6fb8cee652adbe8697e800ea85d3ad3a5be0afc2
HulkyZeus <u20212200102@usco.edu.co>
2024-11-20 18:06:44 -0500

    Arreglo panaderia

--- src/pages/Panaderia.jsx
+++ src/pages/Panaderia.jsx
@@ -19,7 +19,7 @@ import MojiconQueso from '../img/mojiconconqueso.png';
 import PanBatido from '../img/panbatido.png';
 import { useTranslation } from "react-i18next";
 import FondoPan from '../img/FondoPan.webp'
-
+import { useTranslation } from "react-i18next";
 
 
 const cajaDecoracion = {

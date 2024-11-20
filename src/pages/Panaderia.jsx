72189e6da4021b3671023580e1ed0fb9d71f77d4
HulkyZeus <u20212200102@usco.edu.co>
2024-11-20 18:06:44 -0500

    Arreglo panaderia

--- src/pages/Panaderia.jsx
+++ src/pages/Panaderia.jsx
@@ -18,6 +18,7 @@ import PanAliñado from '../img/panaliñado.png';
 import MojiconQueso from '../img/mojiconconqueso.png';
 import PanBatido from '../img/panbatido.png';
 import FondoPan from '../img/FondoPan.webp'
+import { useTranslation } from "react-i18next";
 
 
 
